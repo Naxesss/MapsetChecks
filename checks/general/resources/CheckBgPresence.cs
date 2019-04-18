297e0bd47508a8dbbf415e31e283ab7cde6a6676
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-04-18 19:56:40 +0200

    Add background presence check

---
+++ checks/general/resources/CheckBgPresence.cs
@@ -0,0 +1,76 @@
+﻿using MapsetParser.objects;
+using MapsetParser.objects.events;
+using MapsetVerifier;
+using MapsetVerifier.objects;
+using MapsetVerifier.objects.metadata;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MapsetChecks.checks.general.files
+{
+    public class CheckBgPresence : GeneralCheck
+    {
+        public override CheckMetadata GetMetadata() => new CheckMetadata()
+        {
+            Category = "Metadata",
+            Message = "Missing background.",
+            Author = "Naxess"
+        };
+        
+        public override Dictionary<string, IssueTemplate> GetTemplates()
+        {
+            return new Dictionary<string, IssueTemplate>()
+            {
+                { "All",
+                    new IssueTemplate(Issue.Level.Unrankable,
+                        "All difficulties are missing backgrounds.")
+                    .WithCause(
+                        "None of the difficulties have a background present.") },
+
+                { "One",
+                    new IssueTemplate(Issue.Level.Unrankable,
+                        "{0} has no background.",
+                        "difficulty")
+                    .WithCause(
+                        "One or more difficulties are missing backgrounds, but not all.") },
+
+                { "Missing",
+                    new IssueTemplate(Issue.Level.Unrankable,
+                        "{0} is missing its background file, \"{1}\".",
+                        "difficulty", "path")
+                    .WithCause(
+                        "A background file path is present, but no file exists where it is pointing.") }
+            };
+        }
+
+        public override IEnumerable<Issue> GetIssues(BeatmapSet beatmapSet)
+        {
+            if (beatmapSet.beatmaps.All(aBeatmap => aBeatmap.backgrounds.Count == 0))
+                yield return new Issue(GetTemplate("All"), null);
+            else
+            {
+                foreach (Beatmap beatmap in beatmapSet.beatmaps)
+                {
+                    if (beatmap.backgrounds.Count == 0)
+                        yield return new Issue(GetTemplate("One"), null,
+                            beatmap.metadataSettings.version);
+
+                    else if (beatmapSet.songPath != null)
+                    {
+                        foreach (Background bg in beatmap.backgrounds)
+                        {
+                            string path = beatmapSet.songPath + "\\" + bg.path;
+                            if (!File.Exists(path))
+                                yield return new Issue(GetTemplate("Missing"), null,
+                                    beatmap.metadataSettings.version, bg.path);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
