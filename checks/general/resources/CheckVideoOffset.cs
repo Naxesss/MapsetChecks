3e066bc0f041ede301026f005aa40ab05686109f
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-04-18 20:56:31 +0200

    Add inconsistent video offset check

---
+++ checks/general/resources/CheckVideoOffset.cs
@@ -0,0 +1,48 @@
+﻿using MapsetParser.objects;
+using MapsetParser.objects.events;
+using MapsetVerifier;
+using MapsetVerifier.objects;
+using MapsetVerifier.objects.metadata;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MapsetChecks.checks.general.files
+{
+    public class CheckVideoOffset : GeneralCheck
+    {
+        public override CheckMetadata GetMetadata() => new CheckMetadata()
+        {
+            Category = "Metadata",
+            Message = "Inconsistent video offset.",
+            Author = "Naxess"
+        };
+        
+        public override Dictionary<string, IssueTemplate> GetTemplates()
+        {
+            return new Dictionary<string, IssueTemplate>()
+            {
+                { "Multiple",
+                    new IssueTemplate(Issue.Level.Unrankable,
+                        "{0}",
+                        "video offset : difficulties")
+                    .WithCause(
+                        "There is more than one video offset used between all difficulties.") }
+            };
+        }
+
+        public override IEnumerable<Issue> GetIssues(BeatmapSet aBeatmapSet)
+        {
+            foreach (Issue issue in Common.GetInconsistencies(
+                aBeatmapSet,
+                aBeatmap => aBeatmap.videos.Count > 0 ? aBeatmap.videos[0].offset.ToString() : null,
+                GetTemplate("Multiple")))
+            {
+                yield return issue;
+            }
+        }
+    }
+}
