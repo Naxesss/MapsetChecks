3426fe4c1a04372f05870c1d28139577c834cee0
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-04-18 18:41:16 +0200

    Add additional marker check

---
+++ checks/general/metadata/CheckAdditionalMarkers.cs
@@ -0,0 +1,62 @@
+﻿using MapsetParser.objects;
+using MapsetVerifier;
+using MapsetVerifier.objects;
+using MapsetVerifier.objects.metadata;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MapsetChecks.checks.general.files
+{
+    public class CheckAdditionalMarkers : GeneralCheck
+    {
+        public override CheckMetadata GetMetadata() => new CheckMetadata()
+        {
+            Category = "Metadata",
+            Message = "Additional markers in title.",
+            Author = "Naxess"
+        };
+        
+        public override Dictionary<string, IssueTemplate> GetTemplates()
+        {
+            return new Dictionary<string, IssueTemplate>()
+            {
+                { "Romanized",
+                    new IssueTemplate(Issue.Level.Warning,
+                        "Romanized title field, \"{0}\"",
+                        "romanized title")
+                    .WithCause(
+                        "The romanized title field indicates some kind of version.") },
+
+                { "Unicode",
+                    new IssueTemplate(Issue.Level.Warning,
+                        "Romanized title field, \"{0}\"",
+                        "unicode title")
+                    .WithCause(
+                        "Same as for romanized, but unicode instead.") }
+            };
+        }
+
+        public override IEnumerable<Issue> GetIssues(BeatmapSet beatmapSet)
+        {
+            Beatmap beatmap = beatmapSet.beatmaps[0];
+
+            // Matches additional markers, like "Speed up ver." and "Full Version".
+            Regex myRegex = new Regex("(?i)(( |-)ver(\\.|sion)?)(\\))?$");
+
+            if (myRegex.IsMatch(beatmap.metadataSettings.title))
+                yield return new Issue(GetTemplate("Romanized"), null,
+                    beatmap.metadataSettings.title);
+
+            // Unicode fields do not exist in file version 9.
+            if (beatmap.metadataSettings.titleUnicode != null &&
+                myRegex.IsMatch(beatmap.metadataSettings.titleUnicode))
+            {
+                yield return new Issue(GetTemplate("Unicode"), null,
+                    beatmap.metadataSettings.titleUnicode);
+            }
+        }
+    }
+}
