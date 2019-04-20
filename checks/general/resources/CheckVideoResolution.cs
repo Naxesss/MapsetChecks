3125b77383e50d4b48a8b76a73d756f6ebdc7144
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-04-20 02:14:50 +0200

    Add video resolution check

---
+++ checks/general/resources/CheckVideoResolution.cs
@@ -0,0 +1,83 @@
+﻿using MapsetParser.objects;
+using MapsetParser.objects.events;
+using MapsetVerifier;
+using MapsetVerifier.objects;
+using MapsetVerifier.objects.metadata;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MapsetChecks.checks.general.resources
+{
+    public class CheckVideoResolution : GeneralCheck
+    {
+        public override CheckMetadata GetMetadata() => new CheckMetadata()
+        {
+            Category = "Metadata",
+            Message = "Too high resolution video.",
+            Author = "Naxess"
+        };
+        
+        public override Dictionary<string, IssueTemplate> GetTemplates()
+        {
+            return new Dictionary<string, IssueTemplate>()
+            {
+                { "Resolution",
+                    new IssueTemplate(Issue.Level.Unrankable,
+                        "\"{0}\"",
+                        "file name")
+                    .WithCause(
+                        "A video has a width exceeding 1280 pixels or a height exceeding 720 pixels.") },
+                
+                { "Leaves Folder",
+                    new IssueTemplate(Issue.Level.Unrankable,
+                        "\"{0}\" leaves the current song folder, which shouldn't ever happen.",
+                        "file name")
+                    .WithCause(
+                        "The file path of a video starts with two dots.") },
+
+                { "Missing",
+                    new IssueTemplate(Issue.Level.Warning,
+                        "\"{0}\" is missing, so unable to check that.",
+                        "file name", "exception")
+                    .WithCause(
+                        "A video referenced is not present.") },
+
+                { "Exception",
+                    new IssueTemplate(Issue.Level.Error,
+                        "\"{0}\" returned exception \"{1}\", so unable to check that.",
+                        "file name", "exception")
+                    .WithCause(
+                        "An exception occurred trying to parse a video.") }
+            };
+        }
+
+        public override IEnumerable<Issue> GetIssues(BeatmapSet aBeatmapSet)
+        {
+            foreach (Issue issue in Common.GetTagOsuIssues(
+                aBeatmapSet,
+                aBeatmap => aBeatmap.videos.Count > 0 ? aBeatmap.videos.Select(aVideo => aVideo.path) : null,
+                aTemplateArg => GetTemplate(aTemplateArg),
+                aTagFile =>
+                {
+                    // Executes for each non-faulty video file used in one of the beatmaps in the set.
+                    List<Issue> issues = new List<Issue>();
+                    if (aTagFile.file.Properties.VideoWidth > 1280 ||
+                        aTagFile.file.Properties.VideoHeight > 720)
+                    {
+                        issues.Add(new Issue(GetTemplate("Resolution"), null,
+                            aTagFile.templateArgs[0]));
+                    }
+
+                    return issues;
+                }))
+            {
+                // Returns issues from both non-faulty and faulty files.
+                yield return issue;
+            }
+        }
+    }
+}
