00c5c3219606f26610516eef1c1d4132cd131fca
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-05-01 11:17:09 +0200

    Add inconsistent or unset preview check

---
+++ checks/timing/CheckPreview.cs
@@ -0,0 +1,57 @@
+﻿using MapsetParser.objects;
+using MapsetParser.objects.hitobjects;
+using MapsetParser.objects.timinglines;
+using MapsetVerifier;
+using MapsetVerifier.objects;
+using MapsetVerifier.objects.metadata;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MapsetChecks.checks.timing
+{
+    public class CheckPreview : BeatmapSetCheck
+    {
+        public override CheckMetadata GetMetadata() => new BeatmapCheckMetadata()
+        {
+            Category = "Timing",
+            Message = "Inconsistent or unset preview time.",
+            Author = "Naxess"
+        };
+        
+        public override Dictionary<string, IssueTemplate> GetTemplates()
+        {
+            return new Dictionary<string, IssueTemplate>()
+            {
+                { "Not Set",
+                    new IssueTemplate(Issue.Level.Unrankable,
+                        "Preview time is not set.")
+                    .WithCause(
+                        "The preview time of a beatmap is missing.") },
+
+                { "Inconsistent",
+                    new IssueTemplate(Issue.Level.Unrankable,
+                        "Preview time is inconsistent, see {0}.",
+                        "difficulty")
+                    .WithCause(
+                        "The preview time of a beatmap is different from the reference beatmap.") }
+            };
+        }
+
+        public override IEnumerable<Issue> GetIssues(BeatmapSet aBeatmapSet)
+        {
+            Beatmap refBeatmap = aBeatmapSet.beatmaps[0];
+            string version = refBeatmap.metadataSettings.version;
+
+            foreach (Beatmap beatmap in aBeatmapSet.beatmaps)
+            {
+                if (beatmap.generalSettings.previewTime == -1)
+                    yield return new Issue(GetTemplate("Not Set"), beatmap);
+
+                if (beatmap.generalSettings.previewTime != refBeatmap.generalSettings.previewTime)
+                    yield return new Issue(GetTemplate("Inconsistent"), beatmap,
+                        refBeatmap);
+            }
+        }
+    }
+}
