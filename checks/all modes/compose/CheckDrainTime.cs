c08d6534c4367921d8b7618cf903277827ba55c9
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-05-23 20:42:57 +0200

    Add minimum 30s drain time check

---
+++ checks/all modes/compose/CheckDrainTime.cs
@@ -0,0 +1,47 @@
+﻿using MapsetChecks.objects;
+using MapsetParser.objects;
+using MapsetParser.objects.events;
+using MapsetParser.objects.hitobjects;
+using MapsetParser.statics;
+using MapsetVerifier;
+using MapsetVerifier.objects;
+using MapsetVerifier.objects.metadata;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace MapsetChecks.checks.hit_sounds
+{
+    public class CheckDrainTime : BeatmapCheck
+    {
+        public override CheckMetadata GetMetadata() => new BeatmapCheckMetadata()
+        {
+            Category = "Compose",
+            Message = "Too short drain time.",
+            Author = "Naxess"
+        };
+        
+        public override Dictionary<string, IssueTemplate> GetTemplates()
+        {
+            return new Dictionary<string, IssueTemplate>()
+            {
+                { "Unrankable",
+                    new IssueTemplate(Issue.Level.Unrankable,
+                        "Less than 30 seconds of drain time, currently {0}.",
+                        "drain time")
+                    .WithCause(
+                        "The time from the first object to the end of the last object, subtracting any time between two objects " +
+                        "where a break exists, is in total less than 30 seconds.") }
+            };
+        }
+
+        public override IEnumerable<Issue> GetIssues(Beatmap aBeatmap)
+        {
+            if (aBeatmap.GetDrainTime() < 30 * 1000)
+                yield return new Issue(GetTemplate("Unrankable"), aBeatmap,
+                    Timestamp.Get(aBeatmap.GetDrainTime()));
+        }
+    }
+}
