1dd83611fe4bfcbc9a7af8149f09adcc39b7b0e4
Naxess <30292137+Naxesss@users.noreply.github.com>
2019-06-09 02:42:57 +0200

    Undo inconsistent setting problems and expand reasoning for why warnings are used

--- checks/all modes/settings/CheckInconsistentSettings.cs
+++ checks/all modes/settings/CheckInconsistentSettings.cs
@@ -31,10 +31,20 @@ namespace MapsetChecks.checks.settings
                 {
                     "Reasoning",
                     @"
-                    Difficulties in a beatmapset, which should all use the same video or storyboard, would always want to have the same 
+                    Difficulties in a beatmapset using a similar video or storyboard, would likely want to have the same 
                     epilepsy settings since they would share the same reason to have it. Same goes for countdown, letterboxing, 
-                    widescreen support, audio lead-in, etc. Obviously excluding settings that don't apply. Having difficulties all be 
-                    different in terms of noticeable settings would make the set seem less coherent."
+                    widescreen support, audio lead-in, etc. Obviously excluding settings that don't apply.
+                    <br \><br \>
+                    Having difficulties all be different in terms of noticeable settings would make the set seem less coherent, 
+                    but it can be acceptable if it differs thematically in a way that makes it seem intentional, without needing to 
+                    specify that it is, for example one beatmap being old-style with a special skin and countdown while others are 
+                    more modern and exclude this.
+                    <note>
+                        The Ranking Criteria currently states that inconsistent letterboxing within the same mode is disallowed under 
+                        any circumstance, but this is simply because no one has tried ranking a beatmap with a letterbox as one of its 
+                        thematic features. Once someone attempts and succeeds at this, the rule will very likely be changed to account 
+                        for it.
+                    </note>"
                 },
                 {
                     "Specifics",
@@ -43,17 +53,17 @@ namespace MapsetChecks.checks.settings
                     difficulties of the same mode (excluding beatmapset id): 
                     <div style=""margin:8px 16px;"">
                         <div class=""card-detail-icon cross-icon""></div>beatmapset id
-                        <br \><div class=""card-detail-icon cross-icon""></div>countdown speed (if there's enough time to show it, excluded for taiko/mania)
+                        <br \><div class=""card-detail-icon exclamation-icon""></div>countdown speed (if there's enough time to show it, excluded for taiko/mania)
                         <br \><div class=""card-detail-icon exclamation-icon""></div>countdown offset (if there's enough time to show it, excluded for taiko/mania)
-                        <br \><div class=""card-detail-icon cross-icon""></div>countdown (if there's enough time to show it, excluded for taiko/mania)
-                        <br \><div class=""card-detail-icon cross-icon""></div>letterbox (if there are breaks)
-                        <br \><div class=""card-detail-icon cross-icon""></div>widescreen support (if there's an sb)
-                        <br \><div class=""card-detail-icon cross-icon""></div>storyboard
-                        <br \><div class=""card-detail-icon cross-icon""></div>epilepsy warning (if there's an sb or video)
+                        <br \><div class=""card-detail-icon exclamation-icon""></div>countdown (if there's enough time to show it, excluded for taiko/mania)
+                        <br \><div class=""card-detail-icon exclamation-icon""></div>letterbox (if there are breaks)
+                        <br \><div class=""card-detail-icon exclamation-icon""></div>widescreen support (if there's an sb)
+                        <br \><div class=""card-detail-icon exclamation-icon""></div>storyboard presence
+                        <br \><div class=""card-detail-icon exclamation-icon""></div>epilepsy warning (if there's an sb or video)
                         <br \><div class=""card-detail-icon exclamation-icon""></div>audio lead-in
-                        <br \><div class=""card-detail-icon cross-icon""></div>skin preference
-                        <br \><div class=""card-detail-icon cross-icon""></div>storyboard in front of combo fire (if there's a storyboard)
-                        <br \><div class=""card-detail-icon cross-icon""></div>usage of skin sprites in storyboard (if there's a storyboard)
+                        <br \><div class=""card-detail-icon exclamation-icon""></div>skin preference
+                        <br \><div class=""card-detail-icon exclamation-icon""></div>storyboard in front of combo fire (if there's a storyboard)
+                        <br \><div class=""card-detail-icon exclamation-icon""></div>usage of skin sprites in storyboard (if there's a storyboard)
                         <br \><div class=""card-detail-icon minor-icon""></div>slider tick rate
                     </div>"
                 }
@@ -106,7 +116,7 @@ namespace MapsetChecks.checks.settings
                             if (beatmap.GetCountdownStartBeat() >= 0 && otherBeatmap.GetCountdownStartBeat() >= 0)
                             {
                                 if (beatmap.generalSettings.countdown != otherBeatmap.generalSettings.countdown)
-                                    yield return new Issue(GetTemplate("Unrankable"), beatmap,
+                                    yield return new Issue(GetTemplate("Warning"), beatmap,
                                         "countdown speed", otherBeatmap);
 
                                 if (beatmap.generalSettings.countdownBeatOffset != otherBeatmap.generalSettings.countdownBeatOffset)
@@ -116,7 +126,7 @@ namespace MapsetChecks.checks.settings
                             else if (beatmap.GetCountdownStartBeat() >= 0 || otherBeatmap.GetCountdownStartBeat() >= 0)
                             {
                                 if (beatmap.generalSettings.countdown > 0 || otherBeatmap.generalSettings.countdown > 0)
-                                    yield return new Issue(GetTemplate("Unrankable"), beatmap,
+                                    yield return new Issue(GetTemplate("Warning"), beatmap,
                                         "countdown", otherBeatmap);
                             }
                         }
@@ -124,7 +134,7 @@ namespace MapsetChecks.checks.settings
                         if (beatmap.breaks.Count > 0 && otherBeatmap.breaks.Count > 0)
                         {
                             if (beatmap.generalSettings.letterbox != otherBeatmap.generalSettings.letterbox)
-                                yield return new Issue(GetTemplate("Unrankable"), beatmap,
+                                yield return new Issue(GetTemplate("Warning"), beatmap,
                                     "letterbox", otherBeatmap);
                         }
                     }
@@ -135,15 +145,15 @@ namespace MapsetChecks.checks.settings
                         aBeatmapSet.osb != null)
                     {
                         if (beatmap.generalSettings.widescreenSupport != otherBeatmap.generalSettings.widescreenSupport)
-                            yield return new Issue(GetTemplate("Unrankable"), beatmap,
+                            yield return new Issue(GetTemplate("Warning"), beatmap,
                                 "widescreen support", otherBeatmap);
 
                         if (beatmap.generalSettings.storyInFrontOfFire != otherBeatmap.generalSettings.storyInFrontOfFire)
-                            yield return new Issue(GetTemplate("Unrankable"), beatmap,
+                            yield return new Issue(GetTemplate("Warning"), beatmap,
                                 "storyboard in front of combo fire", otherBeatmap);
 
                         if (beatmap.generalSettings.useSkinSprites != otherBeatmap.generalSettings.useSkinSprites)
-                            yield return new Issue(GetTemplate("Unrankable"), beatmap,
+                            yield return new Issue(GetTemplate("Warning"), beatmap,
                                 "usage of skin sprites in storyboard", otherBeatmap);
                     }
                     else if (
@@ -151,7 +161,7 @@ namespace MapsetChecks.checks.settings
                         (beatmap.HasDifficultySpecificStoryboard() ||
                         otherBeatmap.HasDifficultySpecificStoryboard()))
                     {
-                        yield return new Issue(GetTemplate("Unrankable"), beatmap,
+                        yield return new Issue(GetTemplate("Warning"), beatmap,
                             "storyboard", otherBeatmap);
                     }
 
@@ -162,7 +172,7 @@ namespace MapsetChecks.checks.settings
                         beatmap.videos.Count > 0 && otherBeatmap.videos.Count > 0)
                     {
                         if (beatmap.generalSettings.epilepsyWarning != otherBeatmap.generalSettings.epilepsyWarning)
-                            yield return new Issue(GetTemplate("Unrankable"), beatmap,
+                            yield return new Issue(GetTemplate("Warning"), beatmap,
                                 "epilepsy warning", otherBeatmap);
                     }
 
@@ -171,7 +181,7 @@ namespace MapsetChecks.checks.settings
                             "audio lead-in", otherBeatmap);
 
                     if (beatmap.generalSettings.skinPreference != otherBeatmap.generalSettings.skinPreference)
-                        yield return new Issue(GetTemplate("Unrankable"), beatmap,
+                        yield return new Issue(GetTemplate("Warning"), beatmap,
                             "skin preference", otherBeatmap);
 
                     if (beatmap.difficultySettings.sliderTickRate != otherBeatmap.difficultySettings.sliderTickRate)
