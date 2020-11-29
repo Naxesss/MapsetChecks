5b97ec1610f19f537933c9f7964f17dc359e7ed5
Naxesss <30292137+Naxesss@users.noreply.github.com>
2020-11-29 23:22:06 +0100

    Add inconsistent bar line check

---
+++ Checks/Taiko/Timing/CheckInconsistentBarLines.cs
@@ -0,0 +1,80 @@
+﻿using MapsetParser.objects;
+using MapsetParser.objects.hitobjects;
+using MapsetParser.objects.timinglines;
+using MapsetParser.statics;
+using MapsetVerifierFramework;
+using MapsetVerifierFramework.objects;
+using MapsetVerifierFramework.objects.attributes;
+using MapsetVerifierFramework.objects.metadata;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace MapsetChecks.Checks.Timing
+{
+    [Check]
+    public class CheckInconsistentBarLines : BeatmapSetCheck
+    {
+        public override CheckMetadata GetMetadata() => new BeatmapCheckMetadata()
+        {
+            Modes = new Beatmap.Mode[]
+            {
+                Beatmap.Mode.Taiko
+            },
+            Category = "Timing",
+            Message = "Inconsistent omitted bar lines.",
+            Author = "Naxess",
+
+            Documentation = new Dictionary<string, string>()
+            {
+                {
+                    "Purpose",
+                    @"
+                    Ensuring that bar lines are consistent between all difficulties."
+                },
+                {
+                    "Reasoning",
+                    @"
+                    Since all difficulties in a set are based around a single song, and bar lines are meant to act as a point of reference
+                    for timing in gameplay, it would make the most sense if all difficulties would use the same bar lines. For this reason,
+                    if one difficulty skips one, others probably should too."
+                }
+            }
+        };
+        
+        public override Dictionary<string, IssueTemplate> GetTemplates()
+        {
+            return new Dictionary<string, IssueTemplate>()
+            {
+                { "Inconsistent",
+                    new IssueTemplate(Issue.Level.Problem,
+                        "{0} Inconsistent omitted bar line, see {1}.",
+                        "timestamp - ", "difficulty")
+                    .WithCause(
+                        "A beatmap does not omit bar line where the reference beatmap does, or visa versa.") }
+            };
+        }
+
+        public override IEnumerable<Issue> GetIssues(BeatmapSet beatmapSet)
+        {
+            IEnumerable<Beatmap> taikoBeatmaps = beatmapSet.beatmaps.Where(beatmap => beatmap.generalSettings.mode == Beatmap.Mode.Taiko);
+            Beatmap refBeatmap = taikoBeatmaps.First();
+            foreach (Beatmap beatmap in taikoBeatmaps)
+            {
+                foreach (UninheritedLine line in refBeatmap.timingLines.OfType<UninheritedLine>())
+                {
+                    UninheritedLine respectiveLine =
+                        beatmap.timingLines.OfType<UninheritedLine>().FirstOrDefault(
+                            otherLine => Timestamp.Round(otherLine.offset) == Timestamp.Round(line.offset));
+
+                    double offset = Timestamp.Round(line.offset);
+
+                    if (line.omitsBarLine != respectiveLine.omitsBarLine)
+                        yield return new Issue(GetTemplate("Inconsistent"), beatmap,
+                            Timestamp.Get(offset), refBeatmap);
+                }
+            }
+        }
+    }
+}
