4ac23151cf3da4b39de7c6fb162dcc11060505ba
Naxess <30292137+Naxesss@users.noreply.github.com>
2021-02-03 22:19:42 +0100

    Add collab support for GDers in tags check
    "X & Y's Normal", for example, should now both check for "X" and "Y" in tags / the creator field separately.
    
    This would previously just check for the entirety of "X & Y" in tags, which isn't right.
    
    Also better divided up the code for readability and fixed the namespace.

--- Checks/AllModes/General/Metadata/CheckGuestTags.cs
+++ Checks/AllModes/General/Metadata/CheckGuestTags.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace MapsetChecks.Checks.General.Metadata
+namespace MapsetChecks.Checks.AllModes.General.Metadata
 {
     [Check]
     public class CheckGuestTags : GeneralCheck
@@ -51,27 +51,48 @@ namespace MapsetChecks.Checks.General.Metadata
             };
         }
 
+        private readonly char[] collabChars = new char[] { ',', '&', '|' };
+        private readonly Regex possessorRegex = new Regex(@"(.+)(?:'s|(s)')", RegexOptions.IgnoreCase);
+
         public override IEnumerable<Issue> GetIssues(BeatmapSet beatmapSet)
         {
-            Regex regex = new Regex(@"(.+)(?:'s|(s)')", RegexOptions.IgnoreCase);
             foreach (Beatmap beatmap in beatmapSet.beatmaps)
             {
-                Match match = regex.Match(beatmap.metadataSettings.version);
-                if (match == null)
-                    continue;
+                foreach (string possessor in GetAllPossessors(beatmap.metadataSettings.version))
+                {
+                    if (beatmap.metadataSettings.IsCoveredByTags(possessor) ||
+                        beatmap.metadataSettings.creator.ToLower() == possessor.ToLower())
+                        continue;
 
-                // e.g. "Alphabet" in "Alphabet's Normal"
-                string possessor = match.Groups[1].Value;
-                if (match.Groups.Count > 2)
-                    // If e.g. "Naxess' Insane", group 1 is "Naxes" and group 2 is the remaining "s".
-                    possessor += match.Groups[2].Value;
-
-                if (beatmap.metadataSettings.IsCoveredByTags(possessor))
-                    continue;
-
-                yield return new Issue(GetTemplate("Warning"), null,
-                    beatmap.metadataSettings.version, possessor.ToLower().Replace(" ", "_"));
+                    yield return new Issue(GetTemplate("Warning"), null,
+                        beatmap.metadataSettings.version, possessor.ToLower().Replace(" ", "_"));
+                }
             }
+        }
+
+        /// <summary> Returns any text before the last apostrophe with a neighbouring "s".
+        /// E.g. "Naxess' & Greaper" when inputting "Naxess' & Greaper's Nor'mal". </summary>
+        private string GetPossessor(string text)
+        {
+            Match match = possessorRegex.Match(text);
+            if (match == null)
+                return null;
+
+            // e.g. "Alphabet" in "Alphabet's Normal"
+            string possessor = match.Groups[1].Value;
+            if (match.Groups.Count > 2)
+                // If e.g. "Naxess' Insane", group 1 is "Naxes" and group 2 is the remaining "s".
+                possessor += match.Groups[2].Value;
+
+            return possessor.Trim();
+        }
+
+        /// <summary> Returns all possessors in the given text. E.g. "Naxess', Greaper's &
+        /// Someone else's Normal" returns "Naxess", "Greaper", and "Someone else". </summary>
+        private IEnumerable<string> GetAllPossessors(string text)
+        {
+            foreach (string possessor in text.Split(collabChars))
+                yield return GetPossessor(possessor);
         }
     }
 }
