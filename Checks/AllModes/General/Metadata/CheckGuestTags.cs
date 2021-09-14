a820c27784f9f4008f4876065fb6ef6499ac63fd
Naxesss <30292137+Naxesss@users.noreply.github.com>
2021-09-14 04:11:01 +0200

    Add `/` to collab separators

--- Checks/AllModes/General/Metadata/CheckGuestTags.cs
+++ Checks/AllModes/General/Metadata/CheckGuestTags.cs
@@ -51,7 +51,7 @@ namespace MapsetChecks.Checks.AllModes.General.Metadata
             };
         }
 
-        private readonly char[] collabChars = new char[] { ',', '&', '|' };
+        private readonly char[] collabChars = new char[] { ',', '&', '|', '/' };
         private readonly Regex possessorRegex = new Regex(@"(.+)(?:'s|(s)')", RegexOptions.IgnoreCase);
 
         public override IEnumerable<Issue> GetIssues(BeatmapSet beatmapSet)
